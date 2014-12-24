8d660390ff3ddca437fdf8bf35e88a23994800f2
hal <hal.tillion@gmail.com>
2014-12-24 17:18:25 +0500

    Приготовления к обновлению страницы настроек

--- js/background.js
+++ js/background.js
@@ -1,38 +1,4 @@
 /*Данный код будет выполняться сразу после запуска браузера*/
-function backgroundCheckNotification() {
-    checkNotification('background');
-}
-
-var intervalID;
-var currentUpdateInterval;
-function checkSettingAndModifyBackgroundInterval() {
-    console.log("summon checkSettingAndModifyBackgroundInterval");
-    var updateInterval = localStorage["updateInterval"];
-    console.log("updateInterval " + updateInterval);
-    if (updateInterval == 'never') {
-        if (intervalID != null) {
-            clearInterval(intervalID);
-            currentUpdateInterval = "-1";
-            console.log("stop autoupdate");
-        }
-    } else {
-        console.log("currentUpdateInterval " + currentUpdateInterval);
-        if (updateInterval != currentUpdateInterval) {
-            clearInterval(intervalID);
-            currentUpdateInterval = updateInterval;
-            intervalID = setInterval(backgroundCheckNotification, updateInterval * 1000);
-            console.log("recreate autoupdate");
-        }
-    }
-}
-
-$(document).ready(function () {
-    setNotificationIcon('processing', '?');
-    backgroundCheckNotification();
-    setDefaultStorage();
-    setInterval(checkSettingAndModifyBackgroundInterval, 60 * 1000);
-});
-
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
     if (request.method == "getLocalStorage")
         sendResponse({data: localStorage[request.key]});
--- js/popup.js
+++
@@ -1,5 +0,0 @@
-/*Данный код будет выполняться по клику левой на иконке расширения*/
-$(document).ready(function () {
-    setNotificationIcon('processing', '?');
-    checkNotification('popup');
-});
